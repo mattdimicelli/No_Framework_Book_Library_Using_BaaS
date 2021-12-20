de80c0f3ba670625543ac10ce50d94497d11d948
Matt Di Micelli <matthewdimicelli@gmail.com>
2021-12-20 17:02:12 -0500

    event listener issue

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-let myLibrary = [{
-    title: 'Harry Potter',
-    author: 'J.K. Rowling',
-    pages: 334,
-    wasRead: true,
-    language: 'English',
-    published: undefined,
-    genre: undefined,
-}];
+let myLibrary = [];
+addBookToLibrary('Harry Potter', 'J.K. Rowling', 334, 'not-read', 'English', undefined, undefined);
+
 
 renderBooks();
 
 function renderBooks() {
     let library = document.querySelector('.library');
     let fragment = new DocumentFragment();
-    myLibrary.forEach(book => {
-        const {title, author, pages, wasRead, language, published, genre} = book;
+    myLibrary.forEach((book, index) => {
+        const {title, author, pages, readStatus, language, published, genre} = book;
         let bookCard = document.createElement('article');
         let bookTitle = document.createElement('h2');
         bookTitle.textContent = title;
@@ -22,9 +16,12 @@ function renderBooks() {
         by.textContent = `By: ${author}`;
         let bookPages = document.createElement('p');
         bookPages.textContent = `Pages: ${pages}`;
-        let wasReadSelect = document.createElement('select');
-        wasReadSelect.id = 'was-read';
-        wasReadSelect.required = true;
+        let readStatusSelect = document.createElement('select');
+        readStatusSelect.id = 'was-read';
+        readStatusSelect.required = true;
+        readStatusSelect.addEventListener('change', function(e) {
+            book.setReadStatus(e, book);
+        });
         let readOption = document.createElement('option');
         readOption.value = 'read';
         readOption.textContent = 'Read';
@@ -34,36 +31,57 @@ function renderBooks() {
         let readingOption = document.createElement('option');
         readingOption.value = 'reading';
         readingOption.textContent = 'Reading';
-        wasReadSelect.append(readOption, notReadOption, readingOption);
+        readStatusSelect.append(readOption, notReadOption, readingOption);
+        readStatusSelect.value = readStatus;
         let bookLanguage = document.createElement('p');
         bookLanguage.textContent = language ? `Language: ${language}` : '';
         let datePublished = document.createElement('p');
         datePublished.textContent = published ? `Published: ${published}` : '';
         let bookGenre = document.createElement('p');
         bookGenre.textContent = genre ? `Genre: ${genre}` : '';
+        bookCard.dataset.id = index;  
+        let removeBtn = document.createElement('button');
+        removeBtn.textContent = '×';
+        removeBtn.addEventListener('click', book.removeBook);
         bookCard.append(
-            bookTitle, by, bookPages, wasReadSelect, bookLanguage, datePublished,
-             bookGenre
+            bookTitle, by, bookPages, readStatusSelect, bookLanguage, datePublished,
+             bookGenre, removeBtn
         );
         fragment.append(bookCard);
     });
     library.append(fragment);
 }
 
-function addBookToLibrary() {
-    let newBook = new Book(title, author, pages, wasRead);
+function addBookToLibrary(title, author, pages, readStatus, language, published, genre) {
+    let newBook = new Book(title, author, pages, readStatus, language, published, genre);
     myLibrary.push(newBook);
 }
 
-function Book(title, author, pages, wasRead, language, published, genre) {
+function Book(title, author, pages, readStatus, language, published, genre) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.readStatus = readStatus;
+    this.language = language;
+    this.published = published;
+    this.genre = genre;
 }
 
 Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages}, ${this.wasRead ? 
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.readStatus ? 
         'read' : 'not yet read'}`;
 }
+Book.prototype.setReadStatus = function(e, book) {
+    const readStatus = e.currentTarget.value;
+    book.readStatus = readStatus;
+}
+Book.prototype.removeBook = function(e) {
+    console.log('fire');
+    let bookCard = e.currentTarget.parentElement;
+    console.log(bookCard);
+    myLibrary.splice(bookCard.dataset.id);
+    bookCard.remove();
+}
+
+
 
